4a21ee10dee3275873a5707caa12db89aa8c0320
instajs <ycwong@instajs.com>
2021-01-20 17:58:00 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,63 @@
+const Pdfmaker = require('pdfmake');
+const Storage = require('@ijstech/storage');
+const Uuid = require('uuid');
+
+const DefaultFonts = {
+    Roboto: {
+      normal: __dirname + '/fonts/Roboto-Regular.ttf',
+      bold: __dirname + '/fonts/Roboto-Medium.ttf',
+      italics: __dirname + '/fonts/Roboto-Italic.ttf',
+      bolditalics: __dirname + '/fonts/Roboto-MediumItalic.ttf'
+    }
+};
+
+function createFile(content, options){
+
+}
+function createPdf(pdfDoc, options) {	
+    options = options || {}
+	return new Promise(function(resolve, reject){        
+		fonts = options.fonts || DefaultFonts
+		var printer = new Pdfmaker(fonts);
+		var doc = printer.createPdfKitDocument(pdfDoc);
+		var chunks = [];
+		var result;
+		doc.on('data', function (chunk) {
+			chunks.push(chunk);
+		});
+		doc.on('end', function () {
+			result = Buffer.concat(chunks);
+			resolve(result);
+		});
+		doc.end();
+	})	
+}
+module.exports = {
+    init: function(options){
+        this.options = options;
+    },
+    plugin: function(vm, ctx, site, options){        
+        vm.injectGlobalObject('_$$pdfEngine', {
+            $$createFile: true,
+            createFile: async function(pdfDoc, options){
+                options = options || {};
+                let buffer = await createPdf(pdfDoc, options);
+                let guid = Uuid.v4();
+                let filePath = site.org.guid + '/' + guid;
+                await Storage.writeFile(filePath, buffer)                
+                return JSON.stringify({
+                    guid: guid,
+                    fileName: options.name || 'file.pdf',
+                    size: buffer.length
+                });
+            }
+        }, ''+ function init(){
+            global.Plugins.PdfEngine = {
+                createFile: async function(pdfDoc, options){
+                    let result = _$$pdfEngine.createFile(pdfDoc, options);                    
+                    return JSON.parse(result)
+                }
+            }
+        } + ';init()')
+    }
+}
